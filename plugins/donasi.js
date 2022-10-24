54ddb3db3c6157d69225c73bacff86c763f1fb4c
animek12 <111511065+animek12@users.noreply.github.com>
2022-10-24 12:28:56 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,13 +2,13 @@ import fs from 'fs';
 let handler = async (m, { conn, text, usedPrefix }) => {
 
     let img = fs.readFileSync('./thumbnail.jpg');
-    let img2 = 'https://telegra.ph/file/53e308873b2f9dc1e9c63.jpg'
+    let img2 = 'https://telegra.ph/file/1bebab4c73c9708448fe6.jpg'
     return conn.sendButton(m.chat, 'Scan QR Di atas\n❏ Donasi minimal 500p :v', donate, img2, [["MENU", usedPrefix + "menu"], ["OWNER", usedPrefix + "owner"]], m, {
         contextInfo: {
             externalAdReply: {
-                mediaUrl: "https://instagram.com/fangzganz",
+                mediaUrl: "https://instagram.com/al_vin.233",
                 mediaType: "VIDEO",
-                title: 'Donasi To FangzXD',
+                title: 'Donasi To Fin',
                 body: bottime,
                 previewType: 0,
                 thumbnail: img
